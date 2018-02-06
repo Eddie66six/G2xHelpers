da65d93266c4e8e13d433a29b6d21ee91bc183e0
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2018-02-06 17:13:45 -0200

    primeiro commit

---
+++ G2xHelpers/G2xString.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace G2xHelpers
+{
+    public static class G2xString
+    {
+        public static bool IsEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email)) return false;
+            return Regex.IsMatch(email, "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$");
+        }
+        public static bool IsNumber(string number)
+        {
+            if (string.IsNullOrEmpty(number)) return false;
+            var rgx = new Regex(@"^-?(\d+)([\.|\,]?\d+)?$");
+            return rgx.Match(number).Success;
+        }
+        public static string LimitChar(string value, int maxChar, string finalChar)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+            return $"{value.Substring(0, (value.Length > maxChar ? maxChar : value.Length))}{finalChar}";
+        }
+    }
+}
---
+++ TesteG2xHelpers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TesteG2xHelpers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(G2xHelpers.G2xString.IsEmail("guilherme@w12.com.br"));
+            Console.WriteLine(G2xHelpers.G2xString.IsNumber("12"));
+            Console.WriteLine(G2xHelpers.G2xString.LimitChar("123adf", 6, "..."));
+            Console.ReadKey();
+        }
+    }
+}
