f3f43e79e0b789a6907a8f176e849cc20fa1f71a
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2018-02-08 17:17:39 -0200

    implementado novos metodos

--- G2xHelpers/G2xString.cs
+++ G2xHelpers/G2xString.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace G2xHelpers
 {
@@ -15,6 +16,14 @@ namespace G2xHelpers
         public static string LimitChar(this string value, int maxChar, string finalChar)
         {
             return string.IsNullOrEmpty(value) ? null : $"{value.Substring(0, (value.Length > maxChar ? maxChar : value.Length))}{finalChar}";
+        }
+        public static string ToCamelCase(this string value)
+        {
+            return string.IsNullOrEmpty(value) ? null : Regex.Replace(value, @"(_|-|\s)(\w)", m=> m.Groups[2].ToString().ToUpper());
+        }
+        public static string RemoveAccents(this string value)
+        {
+            return string.IsNullOrEmpty(value) ? null : Encoding.ASCII.GetString(Encoding.GetEncoding("Cyrillic").GetBytes(value));
         }
     }
 }
--- TesteG2xHelpers/Program.cs
+++ TesteG2xHelpers/Program.cs
@@ -11,6 +11,8 @@ namespace TesteG2xHelpers
             Console.WriteLine("guilherme@w12.com.br".IsEmail());
             Console.WriteLine("12.200,00".IsNumber());
             Console.WriteLine("123adf".LimitChar(6, "..."));
+            Console.WriteLine("lista_clientes.lista clientes-lista".ToCamelCase());
+            Console.WriteLine("Aássée^".RemoveAccents());
             Console.ReadKey();
         }
     }
