b1b7fb1b157f527605818ef6375559603958bfdf
Guilherme rodrigues <jogos.coisas@gmail.com>
2018-02-07 23:18:49 -0200

    Padronizaçao

--- G2xHelpers/G2xString.cs
+++ G2xHelpers/G2xString.cs
@@ -4,13 +4,13 @@ namespace G2xHelpers
 {
     public static class G2xString
     {
-        public static bool IsEmail(this string email)
+        public static bool IsEmail(this string value)
         {
-            return !string.IsNullOrEmpty(email) && Regex.IsMatch(email, "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$");
+            return !string.IsNullOrEmpty(value) && Regex.IsMatch(value, "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$");
         }
-        public static bool IsNumber(this string number)
+        public static bool IsNumber(this string value)
         {
-            return !string.IsNullOrEmpty(number) && new Regex(@"^-?((\d+)|(\d+[\.|\,]?\d+)|(\d{1,3}\.\d{3}\,\d+))$").Match(number).Success;
+            return !string.IsNullOrEmpty(value) && new Regex(@"^-?((\d+)|(\d+[\.|\,]?\d+)|(\d{1,3}\.\d{3}\,\d+))$").Match(value).Success;
         }
         public static string LimitChar(this string value, int maxChar, string finalChar)
         {
