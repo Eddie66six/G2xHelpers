55ea428c0a841c1b8391b5909d7277d2dd0108bf
guilherme rodrigues souza <guilherme-rsouza@hotmail.com>
2018-02-09 10:12:43 -0200

    Inserido nova class para numero inteiro

---
+++ G2xHelpers/G2xInteger.cs
@@ -0,0 +1,18 @@
+﻿namespace G2xHelpers
+{
+    public static class G2xInteger
+    {
+        private static bool _IsEvenNumber(int? value)
+        {
+            return value == null ? false : value.Value % 2 == 0;
+        }
+        public static bool IsEvenNumber(this int value)
+        {
+            return _IsEvenNumber(value);
+        }
+        public static bool IsEvenNumber(this int? value)
+        {
+            return _IsEvenNumber(value);
+        }
+    }
+}
